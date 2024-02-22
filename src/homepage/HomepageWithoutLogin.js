29573a6a4e5230a502153e28d13493fc9f266868
Alma <magnanialma03@gmail.com>
2024-02-22 15:28:00 +0100

    V3

--- src/homepage/HomepageWithoutLogin.js
+++ src/homepage/HomepageWithoutLogin.js
@@ -21,10 +21,12 @@ export default function HomepageWithoutLogin() {
 
     function readOnlyCard(q) {
         return (
-            <div class="card" style={{ backgroundColor: "#E9E9FD" }}>
-                <div class="card-body">
-                    <h5 class="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
+            <div className="row">
+                <div class="card" style={{ backgroundColor: "#E9E9FD" }}>
+                    <div class="card-body">
+                        <h5 class="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
+                    </div>
                 </div>
             </div>
         )
@@ -33,11 +35,22 @@ export default function HomepageWithoutLogin() {
     return (
         <>
             <div className="row">
-                <div className="col-4 d-flex justify-content-center text-center">
+                <div className="col-4 p-4">
                     FILTRI
                 </div>
-                <div className="card" style={{ width: "18rem" }}>
-                    {quests.map(q => readOnlyCard(q))}
+                <div className="col-8 p-4">
+                    <div className="row">
+                        {quests.map(q => (
+                            <div key={q.id} className="col-4 p-2 justify-content-center text-center">
+                                <div className="card" style={{ backgroundColor: "#E9E9FD" }}>
+                                    <div className="card-body">
+                                        <h5 className="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
