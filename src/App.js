65aa721764207a0048547295c4ff8f3296c9f493
Alma <magnanialma03@gmail.com>
2024-02-22 20:06:48 +0100

    v5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomepageWithoutLogin from './homepage/HomepageWithoutLogin';
 import HomepageWithLogin from './homepage/HomepageWithLogin';
-import Navbar from './navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './homepage/Login';
-
-
 
 
 function App() {
