7874e4bc4f11fbd3685a43cedeb7656b82082d46
Alma <magnanialma03@gmail.com>
2024-02-22 16:04:51 +0100

    h

--- src/homepage/HomepageWithoutLogin.js
+++ src/homepage/HomepageWithoutLogin.js
@@ -21,12 +21,10 @@ export default function HomepageWithoutLogin() {
 
     function readOnlyCard(q) {
         return (
-            <div className="row">
-                <div class="card" style={{ backgroundColor: "#E9E9FD" }}>
-                    <div class="card-body">
-                        <h5 class="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
-                    </div>
+            <div class="card" style={{ backgroundColor: "#E9E9FD" }}>
+                <div class="card-body">
+                    <h5 class="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
                 </div>
             </div>
         )
@@ -41,11 +39,10 @@ export default function HomepageWithoutLogin() {
                 <div className="col-8 p-4">
                     <div className="row">
                         {quests.map(q => (
-                            <div key={q.id} className="col-4 p-2 justify-content-center text-center">
+                            <div key={q.id} className="col-4 p-2 ">
                                 <div className="card" style={{ backgroundColor: "#E9E9FD" }}>
-                                    <div className="card-body">
-                                        <h5 className="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
-                                        <h6 className="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
+                                    <div className="justify-content-center text-center">
+                                        {readOnlyCard(q)}
                                     </div>
                                 </div>
                             </div>
