588af04ad00aed7e7c61a2b5e33f9bbfd046fd18
Alma <magnanialma03@gmail.com>
2024-02-22 15:19:57 +0100

    V2

--- src/homepage/HomepageWithoutLogin.js
+++ src/homepage/HomepageWithoutLogin.js
@@ -1,18 +1,48 @@
 import Navbar from "../navbar/Navbar"
-
-const HomepageWithoutLogin = (props)=>
-{
-    <>
-        <Navbar/>
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
-   
-    
-    </>
 
+export default function HomepageWithoutLogin() {
+    const [quests, setQuests] = useState([]);
+
+
+    useEffect(
+        () => {
+            axios.get("/api/quest").then(
+                (response) => {
+                    setQuests(response.data);
+                }
+            )
+        },
+        []
+    );
+
+    function readOnlyCard(q) {
+        return (
+            <div class="card" style={{ backgroundColor: "#E9E9FD" }}>
+                <div class="card-body">
+                    <h5 class="card-title"> Type: {q.type} <br /> Reward: {q.reward}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Area: {q.area} <br /> Status: {q.status}</h6>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <div className="row">
+                <div className="col-4 d-flex justify-content-center text-center">
+                    FILTRI
+                </div>
+                <div className="card" style={{ width: "18rem" }}>
+                    {quests.map(q => readOnlyCard(q))}
+                </div>
+            </div>
+        </>
+    )
 
 
 
 }
-
-export default HomepageWithoutLogin;
--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,39 +1,24 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = (props) =>
-{
+const Navbar = (props) => {
 
-    return(
+    return (
         <>
             <div className="row">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary col-4 p-4">
-                <div className="container-fluid ">
-                            <Link className="navbar-brand" to="/">
-                            All Quests
-                            </Link>
-                            <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavAltMarkup"
-                            aria-controls="navbarNavAltMarkup"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                            >
+                <nav className="navbar navbar-expand-lg bg-body-tertiary col-12 p-4">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/">All Quests</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon" />
-                            </button>
-                            
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav col-4 p-4">
-                                <Link className="nav-link active" aria-current="page" to="/">
-                                LOGIN
-                                </Link>
+                        </button>
+                        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <Link className="nav-link active" aria-current="page" to="/">LOGIN</Link>
                             </div>
                         </div>
-                </div>
-            </nav>
-
-                </div>
+                    </div>
+                </nav>
+            </div>
 
         </>
     );
